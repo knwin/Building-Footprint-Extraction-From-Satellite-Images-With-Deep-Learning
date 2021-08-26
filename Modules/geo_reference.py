cb51ce3820d25905124e0ff54bba0a682cfefa65
Zaw Thu Htet( Toby ) <60089444+tobyzawthuhtet@users.noreply.github.com>
2021-08-26 09:19:20 +0630

    Update geo_reference.py

--- Modules/geo_reference.py
+++ Modules/geo_reference.py
@@ -1,63 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# Toby's properties
 
 
-import shutil
-from osgeo import gdal
-import os 
-import glob
-import osr
-
-
-# In[3]:
-
-
-os.chdir(r"C:\Users\Toby\Desktop\Building-Footprint-Extraction-Practical-Project-toby\Building-Footprint-Extraction-Practical-Project-toby\dataset\Annotation Data\Labelme Output data")
-
-
-# In[13]:
 
 
 import shutil
 from osgeo import gdal 
 import osr
 
-tiff = gdal.Open(r"Clip_final_hlaing_thar_yar1.tif")
-gt = tiff.GetGeoTransform()
-
-inputImage = r"test_label.png"
-outputImage = r"label_GCS_projected1.tif"
-
-dataset = gdal.Open(inputImage) 
-I = dataset.ReadAsArray(0,0,dataset.RasterXSize,dataset.RasterYSize)
-
-
-outdataset = gdal.GetDriverByName('GTiff') 
-output_SRS = osr.SpatialReference() 
-output_SRS.ImportFromEPSG(32647)
-outdataset = outdataset.Create(outputImage,dataset.RasterXSize,dataset.RasterYSize,1)                  
-outdataset.GetRasterBand(1).WriteArray(I)
-
-                     
-#gcp_list = [] 
-#gcp_list.append(gdal.GCP(west, north, 0, 0,650))
-#gcp_list.append(gdal.GCP(east,south, 0, 650,0))
-
-outdataset.SetProjection(output_SRS.ExportToWkt()) 
-outdataset.SetGeoTransform(gt)
-wkt = outdataset.GetProjection() 
-#outdataset.SetGCPs(gcp_list,wkt)
-srs =gdal.WarpOptions(dstSRS='ESPG:32647')
-gdal.Warp(outputImage, outdataset)
-print(type(outdataset))
-#outdataset = None
-
-#Final Georeferencing Code
-
-
-# In[15]:
 
 
 def geo_ref(ref_image_dir,target_image_dir,output_image_dir,crs):
@@ -103,45 +55,6 @@ def geo_ref(ref_image_dir,target_image_dir,output_image_dir,crs):
     return(outdata)
     
 
-
-# In[6]:
-
-
-import glob as glob
-data = glob.glob('*.tif')
-print(data)
-
-    
-
-
-# In[16]:
-
-
-geo_ref('Clip_final_hlaing_thar_yar1.tif','test_label.png','test_output2.tif','ESPG:32647')
-
-
-# In[17]:
-
-
-import numpy as np
-outdata= outdataset.ReadAsArray()
-
-
-# In[22]:
-
-
-import matplotlib.pyplot as plt
-plt.imshow(outdata)
-plt.show()
-
-
-# In[13]:
-
-
-help(geo_ref)
-
-
-# In[ ]:
 
 
 
