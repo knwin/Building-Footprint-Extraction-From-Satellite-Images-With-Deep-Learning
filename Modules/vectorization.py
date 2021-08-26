22b1da2290d325a8c83f5c44bb68935ac0b165d9
Zaw Thu Htet( Toby ) <60089444+tobyzawthuhtet@users.noreply.github.com>
2021-08-26 09:07:44 +0630

    Add files via upload

---
+++ Modules/vectorization.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+from osgeo import gdal, ogr
+import sys
+import os
+import matplotlib.pyplot as plt
+
+
+# In[4]:
+
+
+def to_shapefile(imageDir,outputShapefile):
+    """
+    Arguments :
+        imageDir(str) : input geotif image
+        outputShapefile(str) : output shapefile 
+
+    Returns:
+        null
+        
+    """
+    
+    #Vectorization 
+    sourceRaster = gdal.Open(imgDir)
+    band = sourceRaster.GetRasterBand(1)
+    bandArray = band.ReadAsArray(1)
+    outShapefile = outputShapefile
+    driver = ogr.GetDriverByName("ESRI Shapefile")
+    outDatasource = driver.CreateDataSource(outShapefile+ ".shp")
+    outLayer = outDatasource.CreateLayer("polygonized", srs=None)
+    gdal.Polygonize( band, None, outLayer, -1, [], callback=None )
+    outDatasource.Destroy()
+    sourceRaster = None
+
+    
+
+
+# In[ ]:
+
+
+
+
