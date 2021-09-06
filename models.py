19a14280a20a38b1ddb77463ff1e9bae067806d0
Aye-Nyein-Thaw <72487516+Aye-Nyein-Thaw@users.noreply.github.com>
2021-09-07 01:05:14 +0630

    Added Train notebook, dataset, loss, model py files
    Added 4 files
    2. Train.ipynb for training and saving models
    dataset.py that contains Sequence subclass for loading data
    loss.py that contains bce dice loss
    models.py that contains 2 models (UNet and Deep UNet)

---
+++ models.py
@@ -0,0 +1,198 @@
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate, Conv2DTranspose, BatchNormalization, Dropout, Lambda
+
+
+def UNet(num_classes = 1,
+         image_size = 512, 
+         img_channels = 3,
+         activation_fn = 'sigmoid'
+        ):
+
+    inputs = Input((image_size, image_size, img_channels))
+    s = Lambda(lambda x: x / 255)(inputs)
+
+    #Contraction path
+    c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(s)
+    c1 = Dropout(0.1)(c1)
+    c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
+    p1 = MaxPooling2D((2, 2))(c1)
+
+    c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
+    c2 = Dropout(0.1)(c2)
+    c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
+    p2 = MaxPooling2D((2, 2))(c2)
+
+    c3 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
+    c3 = Dropout(0.2)(c3)
+    c3 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
+    p3 = MaxPooling2D((2, 2))(c3)
+
+    c4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
+    c4 = Dropout(0.2)(c4)
+    c4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
+    p4 = MaxPooling2D(pool_size=(2, 2))(c4)
+
+    c5 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
+    c5 = Dropout(0.3)(c5)
+    c5 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
+
+    #Expansive path 
+    u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
+    u6 = concatenate([u6, c4])
+    c6 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
+    c6 = Dropout(0.2)(c6)
+    c6 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
+
+    u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
+    u7 = concatenate([u7, c3])
+    c7 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
+    c7 = Dropout(0.2)(c7)
+    c7 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
+
+    u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
+    u8 = concatenate([u8, c2])
+    c8 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
+    c8 = Dropout(0.1)(c8)
+    c8 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
+
+    u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
+    u9 = concatenate([u9, c1], axis=3)
+    c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
+    c9 = Dropout(0.1)(c9)
+    c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
+
+    outputs = Conv2D(num_classes, (1, 1), activation=activation_fn)(c9)
+    
+    model = Model(inputs=[inputs], outputs=[outputs])
+    
+    return model
+
+
+def DeepUNet(num_classes=1,
+             image_size=512,
+             img_channels=3,
+             activation_fn = 'sigmoid',
+             n_filters_start=32,
+             growth_factor=2,
+             upconv=True
+            ):
+    """
+    Reference - https://github.com/reachsumit/deep-unet-for-satellite-image-segmentation/blob/master/unet_model_deeper.py
+    """
+    
+    droprate=0.25
+    n_filters = n_filters_start
+    inputs = Input((image_size, image_size, img_channels))
+    
+    s = Lambda(lambda x: x / 255)(inputs)
+    
+    conv1 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(s)
+    conv1 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    pool1 = Dropout(droprate)(pool1)
+
+    n_filters *= growth_factor
+    pool1 = BatchNormalization()(pool1)
+    conv2 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(pool1)
+    conv2 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    pool2 = Dropout(droprate)(pool2)
+
+    n_filters *= growth_factor
+    pool2 = BatchNormalization()(pool2)
+    conv3 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(pool2)
+    conv3 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    pool3 = Dropout(droprate)(pool3)
+
+    n_filters *= growth_factor
+    pool3 = BatchNormalization()(pool3)
+    conv4_0 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(pool3)
+    conv4_0 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv4_0)
+    pool4_0 = MaxPooling2D(pool_size=(2, 2))(conv4_0)
+    pool4_0 = Dropout(droprate)(pool4_0)
+
+    n_filters *= growth_factor
+    pool4_0 = BatchNormalization()(pool4_0)
+    conv4_1 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(pool4_0)
+    conv4_1 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv4_1)
+    pool4_1 = MaxPooling2D(pool_size=(2, 2))(conv4_1)
+    pool4_1 = Dropout(droprate)(pool4_1)
+
+    n_filters *= growth_factor
+    pool4_1 = BatchNormalization()(pool4_1)
+    conv4_2 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(pool4_1)
+    conv4_2 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv4_2)
+    pool4_2 = MaxPooling2D(pool_size=(2, 2))(conv4_2)
+    pool4_2 = Dropout(droprate)(pool4_2)
+
+    n_filters *= growth_factor
+    pool4_2 = BatchNormalization()(pool4_2)
+    conv5 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(pool4_2)
+    conv5 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv5)
+    conv5 = Dropout(droprate)(conv5)
+
+    n_filters //= growth_factor
+    if upconv:
+        up6 = concatenate([Conv2DTranspose(n_filters, (2, 2), strides=(2, 2), padding='same')(conv5), conv4_2])
+    else:
+        up6 = concatenate([UpSampling2D(size=(2, 2))(conv5), conv4_2])
+    up6 = BatchNormalization()(up6)
+    conv6 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(up6)
+    conv6 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv6)
+    conv6 = Dropout(droprate)(conv6)
+
+    n_filters //= growth_factor
+    if upconv:
+        up6_1 = concatenate([Conv2DTranspose(n_filters, (2, 2), strides=(2, 2), padding='same')(conv6), conv4_1])
+    else:
+        up6_1 = concatenate([UpSampling2D(size=(2, 2))(conv6), conv4_1])
+    up6_1 = BatchNormalization()(up6_1)
+    conv6_1 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(up6_1)
+    conv6_1 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv6_1)
+    conv6_1 = Dropout(droprate)(conv6_1)
+
+    n_filters //= growth_factor
+    if upconv:
+        up6_2 = concatenate([Conv2DTranspose(n_filters, (2, 2), strides=(2, 2), padding='same')(conv6_1), conv4_0])
+    else:
+        up6_2 = concatenate([UpSampling2D(size=(2, 2))(conv6_1), conv4_0])
+    up6_2 = BatchNormalization()(up6_2)
+    conv6_2 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(up6_2)
+    conv6_2 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv6_2)
+    conv6_2 = Dropout(droprate)(conv6_2)
+
+    n_filters //= growth_factor
+    if upconv:
+        up7 = concatenate([Conv2DTranspose(n_filters, (2, 2), strides=(2, 2), padding='same')(conv6_2), conv3])
+    else:
+        up7 = concatenate([UpSampling2D(size=(2, 2))(conv6_2), conv3])
+    up7 = BatchNormalization()(up7)
+    conv7 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(up7)
+    conv7 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv7)
+    conv7 = Dropout(droprate)(conv7)
+
+    n_filters //= growth_factor
+    if upconv:
+        up8 = concatenate([Conv2DTranspose(n_filters, (2, 2), strides=(2, 2), padding='same')(conv7), conv2])
+    else:
+        up8 = concatenate([UpSampling2D(size=(2, 2))(conv7), conv2])
+    up8 = BatchNormalization()(up8)
+    conv8 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(up8)
+    conv8 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv8)
+    conv8 = Dropout(droprate)(conv8)
+
+    n_filters //= growth_factor
+    if upconv:
+        up9 = concatenate([Conv2DTranspose(n_filters, (2, 2), strides=(2, 2), padding='same')(conv8), conv1])
+    else:
+        up9 = concatenate([UpSampling2D(size=(2, 2))(conv8), conv1])
+    up9 = BatchNormalization()(up9)
+    conv9 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(up9)
+    conv9 = Conv2D(n_filters, (3, 3), activation='relu', padding='same')(conv9)
+
+    conv10 = Conv2D(num_classes, (1, 1), activation=activation_fn)(conv9)
+
+    model = Model(inputs=inputs, outputs=conv10)
+    
+    return model
